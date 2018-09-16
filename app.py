4bfa8a38b8ba0649220abf737e4cb22b3c31cf8c
mgarton1 <mgarton1@iastate.edu>
2018-09-15 20:29:14 -0500

    Add app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return 'Hello world. This is my website.'
+
+if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0')
